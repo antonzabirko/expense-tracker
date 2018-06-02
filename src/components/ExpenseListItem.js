8f5338cd69e6a4485058c8088d543acda5ff1ed3
Anton A. Zabirko <antonzabirko@outlook.com>
2018-06-02 12:18:56 -0400

    Added removable components

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem =  ({ description, amount, createdAt }) => (
+const ExpenseListItem =  ({ dispatch, id, description, amount, createdAt }) => (
     <div>
-        <p>Description: {description} | Amount: {amount} | createdAt: {createdAt}</p>
+        <h3>{description}</h3>
+        <p>Amount: {amount} - createdAt: {createdAt}</p>
+        <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
     </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
