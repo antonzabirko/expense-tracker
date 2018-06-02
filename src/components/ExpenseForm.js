db559e92158699e203a2c89e940b97286beee739
Anton A. Zabirko <antonzabirko@outlook.com>
2018-06-02 15:31:10 -0400

    Updates to expense handling

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -8,14 +8,18 @@ const now = moment();
 console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
-    state ={
-        description: "",
-        textArea: "",
-        amount: "",
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.expense ? props.expense.description : "",
+            textArea: props.expense ? props.expense.textArea : "",
+            amount: props.expense ? (props.expense.amount / 100).toString() : "",
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        };
+    }
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
@@ -51,9 +55,9 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: '' }));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 100,
+                amount: parseFloat(this.state.amount) * 100,
                 createdAt: this.state.createdAt.valueOf(),
-                note: this.state.note
+                note: this.state.textArea
             });
             console.log('submitted');
         }
