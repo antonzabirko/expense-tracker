74a7454434888c0c964fed3158f6f4264fe4a254
Anton A. Zabirko <antonzabirko@outlook.com>
2018-06-09 21:38:12 -0400

    Firebase push data implemented/Added redux-thunk
    Creating an expense now pushes the info to the Firebase database.
    
    Implemented redux-thunk.
    
    ADD_EXPENSE action generator converted to return a function with the help of redux-thunk.
    
    This release is not yet live on heroku.

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This application tracks finances on a daily basis. You can add, remove, and edit
  
 Created with React using Redux alongside [Andrew Mead](https://twitter.com/andrew_j_mead?lang=en)'s course. Full list of dependencies in package.json for now. 
  
+ Please note that the github code is `nightly`, and the Heroku deploy is `versioned`. If you want the latest features: pull, compile, and run `node server/server.js`.
+ 
 Hosted on Heroku as of 6/8! 🎉🎉🎉
 https://expensemap.herokuapp.com/
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import expenses from '../tests/fixtures/expenses';
 
 const config = {
     apiKey: "AIzaSyDPLqlb7K6DvSMx3sF62IBtQ8tk47YHuQc",
@@ -13,22 +14,111 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Anton Zabirko',
-    age: 26,
-    isSingle: true,
-    location: {
-        city: 'New York',
-        country: 'United States'
-    }
-});
+export { firebase, database as default };
 
-// database.ref().set('This is my data');
-
-database.ref('age').set(27);
-database.ref('location/city').set('Philadelphia');
-
-database.ref('attributes').set({
-    height: 75,
-    weight: 185
-});
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+//
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+//
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+//
+// // const onValueChange = database.ref('expenses')
+// //     .on('value', (snapshot) => {
+// //         const expenses = [];
+// //
+// //         snapshot.forEach((childSnapshot) => {
+// //             expenses.push({
+// //                 id: childSnapshot.key,
+// //                 ...childSnapshot.val()
+// //             });
+// //         });
+// //
+// //         console.log(expenses);
+// //     });
+//
+// // database.ref('expenses').push({
+// //     ...expenses[0]
+// // });
+//
+//
+//
+// // database.ref('notes/-LEb7FWeK6tbYIR_4Ef_').remove();
+//
+// // database.ref('notes').push({
+// //     title: 'Course topics',
+// //     body: 'react, angular, python'
+// // });
+//
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}.`)
+// // }, (e) => {
+// //     console.log('Error: ' + e)
+// // });
+//
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //     console.log(snapshot.val());
+// // }, (e) => {
+// //     console.log('Error: ' + e);
+// // });
+// //
+// // setTimeout(() => {
+// //     database.ref('age').set(29);
+// // }, 3500);
+// //
+// // setTimeout(() => {
+// //     database.ref().off(onValueChange);
+// // }, 7000);
+// //
+// // setTimeout(() => {
+// //     database.ref('age').set(30);
+// // }, 10000);
+//
+// // database.ref()
+// //     .once('value')
+// //     .then((snapshot) => {
+// //         const val = snapshot.val();
+// //         console.log(val);
+// //     })
+// //     .catch((err) => {
+// //         console.log(err);
+// //     });
+//
+// // database.ref().set({
+// //     name: 'Anton Zabirko',
+// //     age: 26,
+// //     stressLevel: 6,
+// //     job: {
+// //         title: 'Software Developer',
+// //         company: 'Google'
+// //     },
+// //     location: {
+// //         city: 'New York',
+// //         country: 'United States'
+// //     }
+// // }).then(() => {
+// //     console.log('Data is saved')
+// // }).catch((err) => {
+// //     console.log('Error: ' + err);
+// // });
+// //
+// // database.ref('isSingle').remove().then(() => {
+// //     console.log('isSingle removed!');
+// // }).catch((err) => {
+// //     console.log(err);
+// // });
+// //
+// // database.ref().update({
+// //     stressLevel: 9,
+// //     'job/company': 'Amazon',
+// //     'location/city': 'Seattle'
+// // });
