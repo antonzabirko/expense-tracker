78e42e7506c56cf7e6ef14f1ab72387b9dbdd23b
Anton A. Zabirko <antonzabirko@outlook.com>
2018-06-12 21:44:03 -0400

    Jest testing for auth
    Implemented testing on authentication methods.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from "./actions/auth";
 import getVisibleExpenses from './selectors/expenses';
 import { firebase } from './firebase/firebase';
 //
@@ -31,10 +31,9 @@ const renderApp = () => {
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-
-
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+      store.dispatch(login(user.uid));
       store.dispatch(startSetExpenses()).then(() => {
           renderApp();
           if (history.location.pathname === '/') {
@@ -42,6 +41,7 @@ firebase.auth().onAuthStateChanged((user) => {
           }
       });
   } else {
+      store.dispatch(logout());
       renderApp();
       history.push('/');
   }
