34746b3465e0aaf69a8306edd973116bcc77b280
Anton A. Zabirko <antonzabirko@outlook.com>
2018-06-22 11:19:26 -0400

    1.2.4
    Fixed babel/ESLint integration.
    
    Fixed some code formatting.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { startAddExpense } from "../actions/expenses";
+import { startAddExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-      this.props.startAddExpense(expense);
-      this.props.history.push('/');
+    const { startAddExpenseConnect, history: { push } } = this.props;
+
+    startAddExpenseConnect(expense);
+    push('/');
   };
+
   render() {
     return (
-        <div>
-            <div className="page-header">
-                <div className="content-container">
-                    <h1 className="page-header__title">Add Expense</h1>
-                </div>
-            </div>
-            <div className="content-container">
-                <ExpenseForm
-                    onSubmit={this.onSubmit}
-                />
-            </div>
+      <div>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">
+              Add Expense
+            </h1>
+          </div>
         </div>
+        <div className="content-container">
+          <ExpenseForm
+            onSubmit={this.onSubmit}
+          />
+        </div>
+      </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-    startAddExpense: (expense) => dispatch(startAddExpense(expense))
+const mapDispatchToProps = dispatch => ({
+  startAddExpenseConnect: expense => dispatch(startAddExpense(expense)),
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
